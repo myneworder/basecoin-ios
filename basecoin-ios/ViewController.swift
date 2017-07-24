bc755033fc68cddf5786820bf324ff79c4396bbc
Adrian Brink <adrian@brink-holdings.com>
2017-07-24 19:00:57 -0400

    FFI working

--- basecoin-ios/ViewController.swift
+++ basecoin-ios/ViewController.swift
@@ -6,18 +6,42 @@
 //  Copyright © 2017 Adrian Brink. All rights reserved.
 //
 
+import Bindings
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = UIColor.blue
+        
+        testFFI()
+        
+        let directory = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
+        print(directory)
+        
+        let client = BindingsClient()
+        client.setDirectoryPath(directory[0])
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    private func testFFI() {
+        let coin = BindingsGetCoin()
+        print(coin?.amount())
+        
+        let account = BindingsGetAccount("1B1BE55F969F54064628A63B9559E7C21C925165", "empty")
+        
+        if (account?.error().isEmpty)! {
+            print(account?.height())
+            print(account?.key())
+        } else {
+            print(account?.error())
+        }
     }
 
 
